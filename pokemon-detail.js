a3d36837b1179ccc08250c1618eae325f416eaad
westontolbert1 <124095357+westontolbert1@users.noreply.github.com>
2024-08-13 08:17:07 -0700

    Update pokemon-detail.js

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -1,7 +1,7 @@
 let currentPokemonId = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const MAX_POKEMONS = 12000;
+  const MAX_POKEMONS = 10000000;
   const pokemonID = new URLSearchParams(window.location.search).get("id");
   const id = parseInt(pokemonID, 10);
 
